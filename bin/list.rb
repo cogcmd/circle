d9cace2dc533548e6abc90ed04ee3557d3342b99
Jason Draper <jason@drapergeek.com>
2016-05-09 10:07:49 -0400

    Pass in a project name to list

--- bin/list.rb
+++ bin/list.rb
@@ -3,8 +3,12 @@ require "net/http"
 require "json"
 require_relative "../lib/circle_url"
 
-url = CircleUrl.new("flywheel").generate
-builds = JSON.parse(Net::HTTP.get(url))
-builds.each do |build|
-  puts "#{build["build_num"]} #{build["branch"]}/#{build["committer_name"]} #{build["status"]}"
+if ARGV[0]
+  url = CircleUrl.new(ARGV[0]).generate
+  builds = JSON.parse(Net::HTTP.get(url))
+  builds.each do |build|
+    puts "#{build["build_num"]} #{build["branch"]}/#{build["committer_name"]} #{build["status"]}"
+  end
+else
+  puts "You must provide a project name"
 end
