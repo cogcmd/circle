1402e6c96bd7633b3df576dea3e6af6e518b244d
Kevin Smith <kevin@operable.io>
2016-06-25 16:00:24 -0400

    Implemented body delimiters

--- lib/cog_helpers.rb
+++ lib/cog_helpers.rb
@@ -1,7 +1,8 @@
 class CogHelpers
   def self.output_json_with_template(json, template)
     puts "COG_TEMPLATE: #{template}"
-    puts "JSON\n"
+    puts "JSON"
+    puts ""
     print json
   end
 end
